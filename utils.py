bac8918280c8668413a18de43a7b5065b91007be
Daniel Bar <danielbar@gmail.com>
2022-08-27 13:59:18 +0300

    + Daniel's help

--- utils.py
+++ utils.py
@@ -1,35 +1,105 @@
+import json
 import time
+import os
+import boto3
+import botocore
+import requests
+from botocore.exceptions import ClientError
+from botocore.config import Config
 from yt_dlp import YoutubeDL
 from loguru import logger
+from time import sleep
 
 
-def search_download_youtube_video(video_name, num_results=1):
+def search_download_youtube_video(video_name, num_results, s3_bucket_name):
     """
     This function downloads the first num_results search results from Youtube
     :param video_name: string of the video name
     :param num_results: integer representing how many videos to download
     :return: list of paths to your downloaded video files
     """
-    with YoutubeDL() as ydl:
-        videos = ydl.extract_info(f"ytsearch{num_results}:{video_name}", download=True)['entries']
+    ydl = {'noplaylist': 'True', 'format': 'bestvideo[ext=mp4]+bestaudio[ext=mp4]/mp4', 'outtmpl': '%(id)s.%(ext)s'}
+    # Try to download and return list of video/s or error msg
+    with YoutubeDL(ydl) as ydl:
+        ydl.cache.remove()
+        # 1. get a list of video file names with download=false parameter
+        videos = ydl.extract_info(f"ytsearch{num_results}:{video_name}", download=False)['entries']
+        for video in videos:
+            localprefix = video['id'] + '.mp4'
+            prefix = 'ytdlAppData/' + video['id'] + '.mp4'
+            # check aws s3 bucket for file, then locally and act accordingly,prefix != ydl.prepare_filename(video)
+            if not (check_s3_file(prefix, s3_bucket_name)):
+                if not (os.path.isfile(ydl.prepare_filename(video))):
+                    video_url = video['webpage_url']
+                    ydl.extract_info(video_url, download=True)
+                    upload_file(localprefix, s3_bucket_name)
+                    os.remove(ydl.prepare_filename(video))
+                else:
+                    upload_file(localprefix, s3_bucket_name)
+                    os.remove(ydl.prepare_filename(video))
+            else:
+                if os.path.isfile(ydl.prepare_filename(video)):
+                    # download_file(prefix, s3_bucket_name)
+                    os.remove(ydl.prepare_filename(video))
+            sleep(1)
+        return [ydl.prepare_filename(video) for video in videos]
 
-    return [ydl.prepare_filename(video) for video in videos]
 
-
-def calc_backlog_per_instance(sqs_queue_client, asg_client, asg_group_name):
+def calc_backlog_per_instance(sqs_queue_client, asg_client, asg_group_name, aws_region):
     while True:
         msgs_in_queue = int(sqs_queue_client.attributes.get('ApproximateNumberOfMessages'))
         asg_size = asg_client.describe_auto_scaling_groups(AutoScalingGroupNames=[asg_group_name])['AutoScalingGroups'][0]['DesiredCapacity']
-
         if msgs_in_queue == 0:
             backlog_per_instance = 0
         elif asg_size == 0:
             backlog_per_instance = 99
         else:
             backlog_per_instance = msgs_in_queue / asg_size
-
         logger.info(f'backlog per instance: {backlog_per_instance}')
 
         # TODO send the backlog_per_instance metric to cloudwatch
+        # cloudwatch = boto3.client('cloudwatch', aws_region)
+        # # Put custom metrics
+        # cloudwatch.put_metric_data(
+        #     Namespace='daniel-reuven-monitor-polybot-asg',
+        #     MetricData=[
+        #         {
+        #             'MetricName': 'backlog_per_instance',
+        #             'Value': backlog_per_instance,
+        #             'Unit': 'Count'
+        #         },
+        #     ]
+        # )
+        # time.sleep(60)
 
-        time.sleep(60)
+
+def check_s3_file(key_filename, s3_bucket_name):
+    s3_prefix = 'ytdlAppData/' + key_filename
+    s3 = boto3.resource('s3')
+    try:
+        s3.Object(s3_bucket_name, s3_prefix).load()
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] == "404":
+            # The object does not exist.
+            return False
+        else:
+            # Something else has gone wrong.
+            raise
+    else:
+        # The object does exist.
+        return True
+
+
+def upload_file(key_filename, bucket, object_name=None):
+    s3_prefix = 'ytdlAppData/' + key_filename
+    # Upload the file
+    s3_client = boto3.client('s3')
+    # If S3 object_name was not specified, use key_filename
+    if object_name is None:
+        object_name = s3_prefix
+    try:
+        response = s3_client.upload_file(key_filename, bucket, s3_prefix)
+    except ClientError as e:
+        logger.error(e)
+        return False
+    return True
--- worker.py
+++ worker.py
@@ -1,7 +1,5 @@
 import json
 import time
-import boto3
-import botocore
 from loguru import logger
 from utils import search_download_youtube_video
 
@@ -10,11 +8,9 @@ def process_msg(msg):
     search_download_youtube_video(msg)
 
     # TODO upload the downloaded video to your S3 bucket
-    video = msg
-    s3 = boto3.resource('s3')
-    BUCKET = "firstaccesspoint-rnxo4y7g6jia1k6ki81yrdj1z5ccoeuw2a-s3alias"
-
-    s3.Bucket(BUCKET).upload_file(video, "dump/file")
+    video_filename = search_download_youtube_video(msg, 1, s3_bucket_name)
+    print(f'the video file name is : {video_filename}')
+    return video_filename
 
 
 def main():
